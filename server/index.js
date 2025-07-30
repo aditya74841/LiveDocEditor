81c6bbf001a8f043a752d9d47af8dcfba1bda9e3
Aditya Ranjan <aditya74810@gmail.com>
2025-07-30 20:15:33 +0530

    added home route

--- server/index.js
+++ server/index.js
@@ -141,6 +141,10 @@ app.use(morgan("combined"));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get("/", (_req, res) => res.send("<h1>Server is healthy </h1>"));
+
+
 app.get("/health", (_req, res) => res.send("OK"));
 
 // Create HTTP and Socket.IO servers
